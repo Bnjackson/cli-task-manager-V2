0dc5b0d6c024dbec078d5d8990f2a16f394fc67e
Benjamin <bnjackson951@gmail.com>
2023-09-27 17:55:27 +0100

    Add program introduction and main function

---
+++ index.js
@@ -0,0 +1,21 @@
+'use stict';
+
+console.log(`
+This program is a CLI based task manager, that allows you to view, add, update, or delete tasks all from the command line.
+To get started enter one of the following commands:
+view - view all of your tasks
+add - add a new task
+update - update an already existing task
+delete - delete all or a specific task
+`
+);
+
+async function main() {
+    const anotherTask = true;
+    // The anotherTask variable will remain true while user wishes to add more tasks, when false program ends. Variable is changed by the addAnotherTask function.
+    while (anotherTask) {
+        
+    }
+}
+
+main();
