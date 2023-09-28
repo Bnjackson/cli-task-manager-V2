447157f6b18131487627723f38b24b95830e55ca
Benjamin <bnjackson951@gmail.com>
2023-09-28 17:33:06 +0100

    Add modules for commands, add callCommand function that calls command functions, based on userCommand

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 'use stict';
 
 const inputModule = require('./utils/inputs.js');
+const listModule = require('./commands/list.js');
+const addModule = require('./commands/add.js');
+const updateModule = require('./commands/update.js');
+const deleteModule = require('./commands/delete.js');
 
 console.log(`
-This program is a CLI based task manager, that allows you to view, add, update, or delete tasks all from the command line.
+This program is a CLI based task manager, that allows you to list, add, update, or delete tasks all from the command line.
 To get started enter one of the following commands:
-view - view all of your tasks
+list - list all of your tasks
 add - add a new task
 update - update an already existing task
 delete - delete all or a specific task`);
@@ -15,14 +19,26 @@ async function main() {
     // The anotherTask variable will remain true while user wishes to add more tasks, when false program ends. Variable is changed by the addAnotherTask function.
     while (anotherTask) {
         const userCommand = getUserCommand();
-        console.log(userCommand);
+        callCommand(userCommand);
     }
 }
 
 function getUserCommand() {
-    const allowedInputs = ['view', 'add', 'update', 'delete'];
+    const allowedInputs = ['list', 'add', 'update', 'delete'];
     const message = 'Enter a command: ';
     return inputModule.getUserCommand(message, allowedInputs);
+}
+
+async function callCommand(command) {
+    if (command === 'list') {
+        await listModule.listTasks();
+    } else if (command === 'add') {
+        await addModule.addTask();
+    } else if (command === 'update') {
+        await updateModule.updateTask();
+    } else if (command === 'delete') {
+        await deleteModule.deleteTask();
+    }   
 }
 
 main();
