8efe804423149447618f613cd60fa8872fb6dab5
Benjamin <bnjackson951@gmail.com>
2023-10-01 17:49:07 +0100

    Export express server, require apiRouter containing API endpoints

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const apiRouter = require('./api/api.js');
+
 const app = express();
-const port = 3000;
+app.use('/', apiRouter);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
+
+module.exports = app;
